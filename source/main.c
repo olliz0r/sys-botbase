e5f362053462c7d1dcbbec1d5e4e4bd061220150
jakibaki <jakibaki@live.com>
2018-09-29 07:39:00 +0200

    Fix 6.0

--- source/main.c
+++ source/main.c
@@ -10,7 +10,7 @@
 #include "util.h"
 
 #define TITLE_ID 0x420000000000000F
-#define HEAP_SIZE 0x000540000
+#define HEAP_SIZE 0x000340000
 
 // we aren't an applet
 u32 __nx_applet_type = AppletType_None;
