a5a59e403ec49ba88be6d296910ff2cff691d690
berichan <66521620+berichan@users.noreply.github.com>
2021-01-26 14:52:07 +0000

    Fix wrong thing

--- sys-botbase/source/commands.c
+++ sys-botbase/source/commands.c
@@ -122,7 +122,7 @@ void initController()
         printf("hiddbgInitialize: %d\n", rc);
     // Set the controller type to Pro-Controller, and set the npadInterfaceType.
     controllerDevice.deviceType = HidDeviceType_FullKey3;
-    controllerDevice.npadInterfaceType = XcdInterfaceType_Bluetooth;
+    controllerDevice.npadInterfaceType = HidNpadInterfaceType_Bluetooth;
     // Set the controller colors. The grip colors are for Pro-Controller on [9.0.0+].
     controllerDevice.singleColorBody = RGBA8_MAXALPHA(255,255,255);
     controllerDevice.singleColorButtons = RGBA8_MAXALPHA(0,0,0);
